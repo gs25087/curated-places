7ea10bd6bf581a503320a656835803b4bca38196
Gintare Simutyte <gintare.simutyte@gmail.com>
2023-03-03 16:42:25 +0100

    forms styling

--- src/components/atoms/Avatar/Avatar.tsx
+++ src/components/atoms/Avatar/Avatar.tsx
@@ -61,11 +61,12 @@ export const Avatar: FC<IAvatarProps> = ({ uid, avatarFilename, size, onUpload }
       {avatarUrl ? (
         <Image
           src={avatarUrl}
+          priority
           alt="Avatar"
           className="block h-[6rem] w-[6rem] rounded-full object-cover shadow-lg"
           height={size}
           width={size}
-          style={{ height: size, width: size }}
+          //style={{ height: size, width: size }}
           onClick={() => document.getElementById('single')?.click()}
         />
       ) : (
