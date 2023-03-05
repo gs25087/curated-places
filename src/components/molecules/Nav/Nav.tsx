18a36b8ed866ff90a94decf7f185e13dc93a839a
Gintare Simutyte <gintare.simutyte@gmail.com>
2023-03-05 12:02:50 +0100

    fixed logo white space bug

--- src/components/molecules/Nav/Nav.tsx
+++ src/components/molecules/Nav/Nav.tsx
@@ -22,7 +22,7 @@ export const Nav: FunctionComponent<IProps> = ({ navItems }): JSX.Element => {
   return (
     <nav className="bg-secondary-light dark:bg-secondary-dark fixed top-0 left-1/2 z-nav mx-auto flex w-full -translate-x-1/2 transform flex-wrap items-center justify-between  bg-white md:max-w-md">
       <div className="flex h-navHeight w-full justify-between px-pageMargin">
-        <Link href={'/'} className="min-w-[11rem]">
+        <Link href={'/'} className="whitespace-nowrap ">
           <div className="text-highlight-light color-black flex items-center py-pageMargin pr-3 text-xl transition hover:scale-[0.98]">
             <div className="rounded-full bg-primary px-3 py-0.5 font-medium shadow-md">
               Ar💎chi💫v💕e
